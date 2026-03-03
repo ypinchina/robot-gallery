e8ecbc9d3d04de8dbb7a8931c9619ee98e5ae188
Yip <batmanyp@foxmail.com>
2026-03-03 22:54:18 +0800

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,29 @@ import ShopCart from "./components/ShopCart";
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0);
   const [robotsGalleryList, setRobotsGalleryList] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
   useEffect(() => {
     document.title = `clicked ${count} times`;
   }, [count]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
+    const getFetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await res.json();
+        setLoading(false);
         setRobotsGalleryList(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-      });
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        } else {
+          setError("未知错误");
+        }
+      }
+    };
+    getFetchData();
   }, []);
 
   return (
@@ -40,11 +50,18 @@ const App: React.FC = () => {
         </button>
         <span>计数: {count}</span>
       </div>
-      <div className={style.robotList}>
-        {robotsGalleryList.map((robot) => (
-          <Robots {...robot} key={robot.id} />
-        ))}
-      </div>
+      {
+        error ? '发现错误:' + error : ''
+      }
+      {!loading ? (
+        <div className={style.robotList}>
+          {robotsGalleryList.map((robot) => (
+            <Robots {...robot} key={robot.id} />
+          ))}
+        </div>
+      ) : (
+        <h2>数据加载中...</h2>
+      )}
     </div>
   );
 };
